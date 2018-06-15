0cdf9c1fbc8aa1bc7e9342eedd13b759f53355da
kosuke.makita <kosuke.makita@rakuten.com>
2018-06-15 06:01:06 +0000

    rakuten app

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -21,19 +21,25 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
+   
+   
+   
+   
 
     /**
      * Where to redirect users after registration.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
+  
+  
     public function __construct()
     {
         $this->middleware('guest');
@@ -68,4 +74,5 @@ class RegisterController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
+    protected $redirectTo = '/';
 }

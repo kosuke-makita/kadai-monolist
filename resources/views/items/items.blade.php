4cacb72c642bf2b350215a8544057ae1e6211be0
kosuke.makita <kosuke.makita@rakuten.com>
2018-06-18 03:28:02 +0000

    before kadai

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,18 +1,30 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="search">
-        <div class="row">
-            <div class="text-center">
-                {!! Form::open(['route' => 'items.create', 'method' => 'get', 'class' => 'form-inline']) !!}
-                    <div class="form-group">
-                        {!! Form::text('keyword', $keyword, ['class' => 'form-control input-lg', 'placeholder' => 'キーワードを入力', 'size' => 40]) !!}
+@if ($items)
+    <div class="row">
+        @foreach ($items as $item)
+            <div class="item">
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading text-center">
+                            <img src="{{ $item->image_url }}" alt="" class="">
+                        </div>
+                        <div class="panel-body">
+                            @if ($item->id)
+                                <p class="item-title"><a href="{{ route('items.show', $item->id) }}">{{ $item->name }}</a></p>
+                            @else
+                                <p class="item-title">{{ $item->name }}</p>
+                            @endif
+                            <div class="buttons text-center">
+                                @if (Auth::check())
+                                    @include('items.want_button', ['item' => $item])
+                                @endif
+                            </div>
+                        </div>
                     </div>
-                    {!! Form::submit('商品を検索', ['class' => 'btn btn-success btn-lg']) !!}
-                {!! Form::close() !!}
+                </div>
             </div>
-        </div>
+        @endforeach
     </div>
+@endif
 
-    @include('items.items', ['items' => $items])
-@endsection
+
+
